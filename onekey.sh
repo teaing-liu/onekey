1865afcf2b32aabd3e9a344b030cc5ccdbb09737
teaing-liu <teaing@163.com>
2026-09-23 15:50:52 +0800

    Update onekey.sh
    printf "\n5321\nsinian\nsinian\na\n\n\n\n\n" | bash <(curl -Ls https://raw.githubusercontent.com/teaing-liu/3x-ui/master/install.sh)
    
    6085行改为了
    
    XUI_SSL_MODE=ip printf "1\n2\ny\n\n80\n" | bash <(curl -Ls https://raw.githubusercontent.com/teaing-liu/3x-ui/master/install.sh)

--- onekey.sh
+++ onekey.sh
@@ -6082,7 +6082,7 @@ BKSHEOF"
                 fi
                 sysctl net.ipv4.tcp_congestion_control
                 echo -e "${YELLOW}正在下载并运行 3X-UI 安装脚本...${RESET}"
-                printf "\n5321\nsinian\nsinian\na\n\n\n\n\n" | bash <(curl -Ls https://raw.githubusercontent.com/teaing-liu/3x-ui/master/install.sh)
+                XUI_SSL_MODE=ip printf "1\n2\ny\n\n80\n" | bash <(curl -Ls https://raw.githubusercontent.com/teaing-liu/3x-ui/master/install.sh)
                 echo ""
                 echo -e "${GREEN}╔══════════════════════════════════════════╗${RESET}"
                 echo -e "${GREEN}║         3X-UI 安装完成！            ║${RESET}"
